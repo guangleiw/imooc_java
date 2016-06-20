a9ffec846b61ff6d09fdccc5dbf28d6e49da4705
wangguanglei <wangguanglei1@jd.com>
2016-06-20 08:46:14 +0800

    New version

---
+++ TankWar/src/Blood.java
@@ -0,0 +1,50 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Blood {
+	int x, y, w = 15, h = 15;
+	TankClient tc;
+	int step = 0;
+	boolean live = true;
+	public boolean isLive() {
+		return live;
+	}
+
+	public void setLive(boolean live) {
+		this.live = live;
+	}
+
+	private int pos[][] = { { 350, 300 }, { 360, 300 },{ 375, 275 }, { 400, 200 }, { 360, 270},{ 365, 290}, { 340, 280 }};
+
+	public Blood() {
+		x = pos[0][0];
+		y = pos[0][1];
+	}
+
+	public void draw(Graphics g) {
+		
+		if(!isLive()) return;
+		
+		Color c = g.getColor();
+		g.setColor(Color.MAGENTA);
+		g.fillRect(x, y, w, h);
+		g.setColor(c);
+	
+		move();		
+	}
+
+	private void move() {
+		step ++;
+		if(step  == pos.length){
+			step = 0;
+		}
+		x = pos[step][0];
+		y = pos[step][1];
+	}
+
+	public Rectangle getRect() {
+		// TODO Auto-generated method stub
+		return new Rectangle(x, y, w, h);
+	}
+}
---
+++ TankWar/src/Tank.java
@@ -0,0 +1,370 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Tank {
+
+	private static final int XSPEED = 8;
+	private static final int YSPEED = 8;
+
+	private static final int WIDTH = 30;
+	private static final int HEIGHT = 30;
+
+	private boolean bL = false;
+	private boolean bU = false;
+	private boolean bR = false;
+	private boolean bD = false;
+
+	private boolean good = true;
+	private int life = 100;
+
+	private static Toolkit tk = Toolkit.getDefaultToolkit();
+
+	private static Map<String, Image> hashImages = new HashMap<String, Image>();
+	// int[] diameter = {4, 7, 12, 18, 26, 32, 49, 30, 14, 6};
+	private static Image imgs[] = null;
+
+	static {
+		imgs = new Image[] { tk.getImage(Explode.class.getClassLoader().getResource("images/tankL.gif")),
+				tk.getImage(Explode.class.getClassLoader().getResource("images/tankLU.gif")),
+				tk.getImage(Explode.class.getClassLoader().getResource("images/tankU.gif")),
+				tk.getImage(Explode.class.getClassLoader().getResource("images/tankRU.gif")),
+				tk.getImage(Explode.class.getClassLoader().getResource("images/tankR.gif")),
+				tk.getImage(Explode.class.getClassLoader().getResource("images/tankRD.gif")),
+				tk.getImage(Explode.class.getClassLoader().getResource("images/tankD.gif")),
+				tk.getImage(Explode.class.getClassLoader().getResource("images/tankLD.gif")) };
+
+		hashImages.put("L", imgs[0]);
+		hashImages.put("LU", imgs[1]);
+		hashImages.put("U", imgs[2]);
+		hashImages.put("RU", imgs[3]);
+		hashImages.put("R", imgs[4]);
+		hashImages.put("RD", imgs[5]);
+		hashImages.put("D", imgs[6]);
+		hashImages.put("LD", imgs[7]);
+
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public void setLife(int life) {
+		this.life = life;
+	}
+
+	private Direction dir;
+	private Direction barrelDir = Direction.D;
+	private TankClient tc = null;
+	private boolean live = true;
+	int x, y, oldx, oldy;
+
+	// enum Direction {
+	// L, LU, U, UR, R, RD, D, DL, STOP
+	// };
+
+	private BloodBar bb = new BloodBar();
+
+	public static Random r = new Random();
+	private int step = r.nextInt(12) + 3;
+
+	public Tank(int x, int y, boolean good) {
+		this.x = x;
+		this.y = y;
+		this.setGood(good);
+	}
+
+	public Tank(int x, int y, TankClient tc, boolean good, Direction dir) {
+		this(x, y, good);
+		this.tc = tc;
+		this.dir = dir;
+	}
+
+	// public Tank(int x2, int y2, TankClient tc2, boolean good2, Direction d) {
+	// // TODO Auto-generated constructor stub
+	// }
+
+	public boolean isLive() {
+		return live;
+	}
+
+	public void setLive(boolean live) {
+		this.live = live;
+	}
+
+	public void draw(Graphics g) {
+//		 Color c = g.getColor();
+
+		if (!isLive()) {
+			tc.tanks.remove(this);
+			return;
+		}
+
+		if (isGood()) {
+			g.setColor(Color.WHITE);
+			bb.draw(g);
+		} else {
+			g.setColor(Color.BLUE);
+		}
+		// g.fillOval(x, y, WIDTH, HEIGHT);
+//		 g.setColor(c);
+		 locateDirection();
+
+		switch (barrelDir) {
+		case L:
+			g.drawImage(hashImages.get("L"), x, y, null);
+			break;
+		case LU:
+			g.drawImage(hashImages.get("LU"), x, y, null);
+			break;
+		case U:
+			g.drawImage(hashImages.get("U"), x, y, null);
+			break;
+		case UR:
+			g.drawImage(hashImages.get("RU"), x, y, null);
+			break;
+		case R:
+			g.drawImage(hashImages.get("R"), x, y, null);
+			break;
+		case RD:
+			g.drawImage(hashImages.get("RD"), x, y, null);
+			break;
+		case D:
+			g.drawImage(hashImages.get("D"), x, y, null);
+			break;
+		case DL:
+			g.drawImage(hashImages.get("DL"), x, y, null);
+			break;
+		case STOP:
+			break;
+		}
+
+		if (this.dir != Direction.STOP) {
+			this.barrelDir = this.dir;
+		}
+
+		if (!isGood()) {
+			if (r.nextInt(30) == 0) {
+				this.fire();
+			}
+
+			Direction[] dirs = Direction.values();
+
+			if (step == 0) {
+				int rn = r.nextInt(dirs.length);
+				this.dir = dirs[rn];
+				step = r.nextInt(12) + 3;
+			}
+			step--;
+		}
+		oldx = x;
+		oldy = y;
+		move();
+
+		if (x < 0)
+			x = 0;
+		if (y < 0)
+			y = 0;
+		if (x + Tank.WIDTH > TankClient.GAMEWIDTH)
+			x = TankClient.GAMEWIDTH - Tank.WIDTH;
+		if (y + Tank.HEIGHT > TankClient.GAMEHEIGH)
+			y = TankClient.GAMEHEIGH - Tank.HEIGHT;
+
+	}
+
+	public void move() {
+		// System.out.println("move");
+		switch (dir) {
+		case L:
+			x -= XSPEED;
+			break;
+		case LU:
+			x -= XSPEED;
+			y -= YSPEED;
+			break;
+		case U:
+			y -= XSPEED;
+			break;
+		case UR:
+			x += XSPEED;
+			y -= YSPEED;
+			break;
+		case R:
+			x += XSPEED;
+			break;
+		case RD:
+			x += XSPEED;
+			y += YSPEED;
+			break;
+		case D:
+			y += XSPEED;
+			break;
+		case DL:
+			x -= XSPEED;
+			y += YSPEED;
+			break;
+		case STOP:
+			break;
+		}
+
+	}
+
+	public boolean eat(Blood b) {
+		if (live && b.isLive() && getRect().intersects(b.getRect())) {
+			this.setLife(100);
+			b.setLive(false);
+			return true;
+		}
+		return false;
+	}
+
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_F2:
+			if (!this.live) {
+				this.setLive(true);
+				this.setLife(100);
+			}
+			break;
+		case KeyEvent.VK_CONTROL:
+			fire();
+			break;
+		case KeyEvent.VK_UP:
+			bU = true;
+			break;
+		case KeyEvent.VK_DOWN:
+			bD = true;
+			break;
+		case KeyEvent.VK_LEFT:
+			bL = true;
+			break;
+		case KeyEvent.VK_RIGHT:
+			bR = true;
+			break;
+		case KeyEvent.VK_A:
+			superFire();
+			break;
+		}
+		locateDirection();
+	}
+
+	public void fire() {
+		if (!live)
+			return;
+		int x = this.x + Tank.WIDTH / 2 - Missile.WIDTH / 2;
+		int y = this.y + Tank.HEIGHT / 2 - Missile.HEIGHT / 2;
+
+		tc.missiles.add(new Missile(x, y, this.good, this.barrelDir, this.tc));
+	}
+
+	public void fire(Direction dir) {
+		if (!live)
+			return;
+		int x = this.x + Tank.WIDTH / 2 - Missile.WIDTH / 2;
+		int y = this.y + Tank.HEIGHT / 2 - Missile.HEIGHT / 2;
+
+		tc.missiles.add(new Missile(x, y, this.good, dir, this.tc));
+	}
+
+	public void superFire() {
+		Direction dirs[] = Direction.values();
+		for (int i = 0; i < dirs.length; i++) {
+			if (dirs[i] != Direction.STOP)
+				fire(dirs[i]);
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_UP:
+			bU = false;
+			break;
+		case KeyEvent.VK_DOWN:
+			bD = false;
+			break;
+		case KeyEvent.VK_LEFT:
+			bL = false;
+			break;
+		case KeyEvent.VK_RIGHT:
+			bR = false;
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void locateDirection() {
+		if (bL && !bU && !bR && !bD)
+			dir = Direction.L;
+		else if (bL && bU && !bR && !bD)
+			dir = Direction.LU;
+		else if (!bL && bU && !bR && !bD)
+			dir = Direction.U;
+		else if (!bL && bU && bR && !bD)
+			dir = Direction.UR;
+		else if (!bL && !bU && bR && !bD)
+			dir = Direction.R;
+		else if (!bL && !bU && bR && bD)
+			dir = Direction.RD;
+		else if (!bL && !bU && !bR && bD) {
+			// System.out.println(y);
+			dir = Direction.D;
+		} else if (bL && !bU && !bR && bD)
+			dir = Direction.DL;
+		else if (!bL && !bU && !bR && !bD)
+			dir = Direction.STOP;
+//		barrelDir = dir;
+	}
+
+	public Rectangle getRect() {
+		return new Rectangle(x, y, WIDTH, HEIGHT);
+	}
+
+	public boolean isGood() {
+		return good;
+	}
+
+	public void setGood(boolean good) {
+		this.good = good;
+	}
+
+	public void collidesWithWalls(Wall w) {
+		if (this.getRect().intersects(w.getRect())) {
+			stay();
+		}
+	}
+
+	public void collidesWithTanks(java.util.List<Tank> tanks) {
+		for (int i = 0; i < tanks.size(); i++) {
+			Tank t = tanks.get(i);
+			if (t != this && this.getRect().intersects(t.getRect())) {
+				stay();
+			}
+		}
+	}
+
+	private void stay() {
+		x = oldx;
+		y = oldy;
+	}
+
+	private class BloodBar {
+		public void draw(Graphics g) {
+			Color c = g.getColor();
+			g.setColor(Color.RED);
+			g.drawRect(x, y - 10, WIDTH, 10);
+			int w = WIDTH * life / 100;
+			g.fillRect(x, y - 10, w, 10);
+			g.setColor(c);
+		}
+	}
+
+}

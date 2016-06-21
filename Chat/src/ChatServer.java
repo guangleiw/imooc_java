7d5b50390bd2c5fe57c79b1e80abc5aa1b038c23
guangleiw <stylite.wang@gmail.com>
2016-06-21 23:08:09 +0800

    bug fix for socket close

--- Chat/src/ChatServer.java
+++ Chat/src/ChatServer.java
@@ -5,16 +5,26 @@ import java.net.Socket;
 
 public class ChatServer {
 	static ServerSocket ss;
+	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		boolean started = false;
 		try {
 			ss = new ServerSocket(8710);
-			while (true) {
+			started = true;
+			while (started) {
+				boolean bConnected = false;
 				Socket s = ss.accept();
+				bConnected  = true;
 				DataInputStream dis = new DataInputStream(s.getInputStream());
-				System.out.println(dis.readUTF());
+				while(bConnected){
+					String str  = dis.readUTF();
+					System.out.println(str);
+				}
+				dis.close();
+//				System.out.println(dis.readUTF());
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block

4fe2a72afb8bdf9c6013cede89f417d8581a8589
guangleiw <stylite.wang@gmail.com>
2016-06-21 08:44:19 +0800

    Chat UI

--- Chat/src/ChatClient.java
+++ Chat/src/ChatClient.java
@@ -2,9 +2,18 @@ import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.TextArea;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ChatClient extends Frame {
+	
+	Socket s;
 
 	TextField tfTxt = new TextField();
 	TextArea taContent = new TextArea();
@@ -21,11 +30,53 @@ public class ChatClient extends Frame {
 		add(taContent, BorderLayout.NORTH);
 		pack();
 		this.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent arg0) {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				super.windowClosing(e);
 				System.exit(0);
-			};
+			}
 		});
+		tfTxt.addActionListener(new tfListener());
 		this.setVisible(true);
+		
+		connect();
+	}
+	
+	private void connect(){
+		try {
+			s = new Socket("127.0.0.1",8710);
+System.out.println("connected!");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private class tfListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String str = tfTxt.getText();
+			taContent.setText(str);
+			tfTxt.setText("");
+			DataOutputStream dos;
+			try {
+				dos = new DataOutputStream(s.getOutputStream());
+				dos.writeUTF(str);
+				dos.flush();
+				dos.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+		
 	}
 
 }

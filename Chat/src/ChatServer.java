6f78aaa42394f66a9d100dfdc48467de91c49c31
guangleiw <stylite.wang@gmail.com>
2016-06-22 08:09:51 +0800

    add thread to server

--- Chat/src/ChatServer.java
+++ Chat/src/ChatServer.java
@@ -1,36 +1,90 @@
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
-	static ServerSocket ss;
-	
-
+	ServerSocket ss;
+	// static Socket s = null;
+	DataInputStream dis = null;
+	boolean started = false;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean started = false;
+		new ChatServer().start();
+	}
+
+	public void start() {
 		try {
 			ss = new ServerSocket(8710);
-			started = true;
-			while (started) {
-				boolean bConnected = false;
-				Socket s = ss.accept();
-				bConnected  = true;
-				DataInputStream dis = new DataInputStream(s.getInputStream());
-				while(bConnected){
-					String str  = dis.readUTF();
-					System.out.println(str);
-				}
-				dis.close();
-//				System.out.println(dis.readUTF());
-			}
+		} catch (BindException b) {
+			System.out.println("port is used");
+			System.out.println("Please close the server application before start!");
+			System.exit(0);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("A client is connected!");
+		try {
+			started = true;
+			while (started) {
+				Socket s = ss.accept();
+				Client c = new Client(s);
+				new Thread(c).start();
+				// dis.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ss.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private class Client implements Runnable {
+		private Socket s;
+		private DataInputStream dis = null;
+		boolean bConnected = false;
+
+		public Client(Socket s) {
+			this.s = s;
+			try {
+				dis = new DataInputStream(s.getInputStream());
+				bConnected = true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				while (bConnected) {
+					String str = dis.readUTF();
+					System.out.println(str);
+				}
+			} catch (EOFException e) {
+				System.out.println("Closed");
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (null != dis)
+						dis.close();
+					if (null != s)
+						s.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
